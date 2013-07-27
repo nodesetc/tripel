936dde863ff92922710e56f00264e13e42375991
john martin <nodesetc@gmail.com>
2013-07-27 03:28:24 -0700

    more new UI stuff

--- tripel/static/trpl-ng.js
+++ tripel/static/trpl-ng.js
@@ -4,55 +4,110 @@ var trplApp = angular.module('trpl', ['ngResource', 'ui.state']);
 trplApp.value('trplConstants', {rootPath: '/tripel'});
 
 trplApp.config(
-	function($stateProvider, $routeProvider) {
+	function($stateProvider, $routeProvider, $urlRouterProvider) {
 		$stateProvider
 			.state('appView', { 
 					url: '/app_view', 
 					template: '<toptabs pane-list-type="app-views"></toptabs>',
 					abstract: true
-				}).
-			state('appView.metaspaceCmds', {
+				})
+			.state('appView.metaspaceCmds', {
 					url: '/metaspace_cmds',
 					templateUrl: 'static/ng_partials/metaspace_commands.html'
-				}).
-			state('appView.manageLoggedInUsr', {
+				})
+			.state('appView.metaspaceCmds.nodespaceCreate', {
+					url: '/nodespace_create',
+					templateUrl: 'static/ng_partials/nodespace_edit.html'
+				})
+			.state('appView.metaspaceCmds.userInvitationCreate', {
+					url: '/user_invitation_create',
+					templateUrl: 'static/ng_partials/user_invitation_create.html'
+				})
+			.state('appView.metaspaceCmds.nodespaceListAll', {
+					url: '/nodespaces_all',
+					controller: 'NodespaceListAllCtrl',
+					templateUrl: 'static/ng_partials/nodespace_list.html'
+				})
+			.state('appView.metaspaceCmds.nodespaceListAll.viewNodespace', {
+					url: '/:nodespaceId',
+					controller: 'NodespaceViewCtrl',
+					templateUrl: 'static/ng_partials/nodespace_graph_view.html'
+				})
+			.state('appView.metaspaceCmds.userListAll', {
+					url: '/users',
+					templateUrl: 'static/ng_partials/user_list_metaspace.html'
+				})
+			.state('appView.manageLoggedInUser', {
 					url: '/manage_logged_in_user',
 					templateUrl: 'static/ng_partials/manage_logged_in_user.html'
-				}).
-			state('appView.nodespaceList', {
-					url: '/nodespaces',
-					controller: 'NodespaceListCtrl',
+				})
+			.state('appView.manageLoggedInUser.editUserInfo', {
+					url: '/user_info_edit',
+					templateUrl: 'static/ng_partials/user_info_edit.html'
+				})
+			.state('appView.manageLoggedInUser.changeUserPassword', {
+					url: '/user_change_password',
+					templateUrl: 'static/ng_partials/user_change_password.html'
+				})
+			.state('appView.nodespaceListAccessible', {
+					url: '/nodespaces_accessible',
+					controller: 'NodespaceListAccessibleCtrl',
 					templateUrl: 'static/ng_partials/nodespace_list.html'
+				})
+			.state('appView.nodespaceListAccessible.nodespaceInvitationCreate', {
+					url: '/nodespace_invitation_create',
+					templateUrl: 'static/ng_partials/nodespace_invitation_create.html'
+				})
+			.state('appView.nodespaceListAccessible.viewNodespace', {
+					url: '/:nodespaceId',
+					controller: 'NodespaceViewCtrl',
+					templateUrl: 'static/ng_partials/nodespace_graph_view.html'
 				});
+		
+		$urlRouterProvider.otherwise('/app_view/nodespaces_accessible');
 	}
 );
 
 trplApp.service('trplBackendSvc',
 	function($http, trplConstants) {
 		this.isAllowedToUse = function(pageName, callbackFn) {
-			$http.get(trplConstants.rootPath+'/'+pageName, {params: {modeselektion: 'check_is_allowed_to_use'}}).
-				success(function(data, status, headers, config) {
+			$http.get(trplConstants.rootPath+'/'+pageName, {params: {modeselektion: 'check_is_allowed_to_use'}})
+				.success(function(data, status, headers, config) {
 					var isAllowedToUse = false;
 					if(data.is_allowed_to_use !== undefined) isAllowedToUse = (data.is_allowed_to_use === true);
 					callbackFn(isAllowedToUse);
-				}).
-				error(function(data, status, headers, config) {
+				})
+				.error(function(data, status, headers, config) {
 					callbackFn(false);
 				});
 		};
-		
-		this.getAccessibleNodespaces = function(callbackFn) {
-			$http.get(trplConstants.rootPath+'/nodespace_list_accessible', {params: {modeselektion: 'json'}}).
-				success(function(data, status, headers, config) {
+
+		this.getObjList = function(callbackFn, subUri, params) {
+			reqParams = jQuery.extend({}, params, {modeselektion: 'json'});
+			reqUri = trplConstants.rootPath + subUri;
+			$http.get(reqUri, {params: reqParams})
+				.success(function(data, status, headers, config) {
 					if((data.length !== undefined)) {
 						callbackFn(data);
 					} else {
 						callbackFn([]);
 					}
-				}).
-				error(function(data, status, headers, config) {
+				})
+				.error(function(data, status, headers, config) {
 					callbackFn([]);
 				});
+		};
+
+		this.getAccessibleNodespaces = function(callbackFn) {
+			return this.getObjList(callbackFn, '/nodespace_list_accessible', {});
+		};
+
+		this.getAllNodespaces = function(callbackFn) {
+			return this.getObjList(callbackFn, '/nodespace_list_all', {});
+		};
+		
+		this.getGraphElements = function(callbackFn, nodespaceId) {
+			return this.getObjList(callbackFn, '/nodespace_overview', {nodespace_id: nodespaceId});
 		};
 	}
 );
@@ -67,7 +122,7 @@ trplApp.service('paneListSvc',
 					var urlBase = '#/app_view/';
 					var panes = [{title: 'metaspace commands', url: urlBase+'metaspace_cmds', isSelected: false, isUsable: false},
 							{title: 'manage logged in user', url: urlBase+'manage_logged_in_user', isSelected: false, isUsable: true},
-							{title: 'accessible nodespaces', url: urlBase+'nodespaces', isSelected: true, isUsable: true}];
+							{title: 'accessible nodespaces', url: urlBase+'nodespaces_accessible', isSelected: false, isUsable: true}];
 					
 					var callbackFn = function(isAllowedToUseMetaspaceCmds) {
 						panes[0].isUsable = isAllowedToUseMetaspaceCmds;
@@ -80,9 +135,16 @@ trplApp.service('paneListSvc',
 				case 'metaspace-commands':
 					var urlBase = '#/app_view/metaspace_cmds/';
 					var panes = [{title: 'create new nodespace', url: urlBase+'nodespace_create', isSelected: false, isUsable: true},
-							{title: 'invite new user', url: urlBase+'user_invite_create', isSelected: false, isUsable: true},
-							{title: 'list all nodespaces', url: urlBase+'nodespace_list', isSelected: false, isUsable: true},
-							{title: 'list all users', url: urlBase+'user_list_all', isSelected: false, isUsable: true}];
+							{title: 'invite new user', url: urlBase+'user_invitation_create', isSelected: false, isUsable: true},
+							{title: 'list all nodespaces', url: urlBase+'nodespaces_all', isSelected: false, isUsable: true},
+							{title: 'list all users', url: urlBase+'users', isSelected: false, isUsable: true}];
+					
+					return panes
+				
+				case 'manage-user':
+					var urlBase = '#/app_view/manage_logged_in_user/';
+					var panes = [{title: 'edit user info', url: urlBase+'user_info_edit', isSelected: false, isUsable: true},
+							{title: 'change password', url: urlBase+'user_change_password', isSelected: false, isUsable: true}];
 					
 					return panes
 					
@@ -108,13 +170,39 @@ trplApp.controller('SelectPaneCtrl',
 	}
 );
 
-trplApp.controller('NodespaceListCtrl',
+trplApp.controller('NodespaceListAllCtrl',
 	function($scope, trplBackendSvc) {
+		$scope.urlBase = '#/app_view/metaspace_cmds/nodespaces_all/';
+		
+		var nodespaceListData = $scope.nodespaceListData = {};
+		var callbackFn = function(newNSList) {
+			nodespaceListData.nodespaceList = newNSList;
+		}
+		trplBackendSvc.getAllNodespaces(callbackFn);
+	}
+);
+
+trplApp.controller('NodespaceListAccessibleCtrl',
+	function($scope, trplBackendSvc) {
+		$scope.urlBase = '#/app_view/nodespaces_accessible/';
+		
 		var nodespaceListData = $scope.nodespaceListData = {};
 		var callbackFn = function(newNSList) {
 			nodespaceListData.nodespaceList = newNSList;
 		}
 		trplBackendSvc.getAccessibleNodespaces(callbackFn);
+	}
+);
+
+trplApp.controller('NodespaceViewCtrl',
+	function($scope, $stateParams, trplBackendSvc) {
+		var nodespaceViewData = $scope.nodespaceViewData = {};
+		var callbackFn = function(newGraphElements) {
+			nodespaceViewData.graphElements = newGraphElements;
+			var nsGraph = angular.element("#ns_graph").cytoscape("get");
+			nsGraph.load(nodespaceViewData.graphElements);
+		}
+		trplBackendSvc.getGraphElements(callbackFn, $stateParams.nodespaceId);
 	}
 );
 
