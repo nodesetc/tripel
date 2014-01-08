558b7881b0b364e0f1d280e13aa6f2e760673f0e
john martin <nodesetc@gmail.com>
2014-01-08 04:05:18 -0800

    added the following functionality: list all users, edit logged in user info, change logged in user password.  refactoring of http requests in trplBackendSvc, minor refactoring of node browsing

--- tripel/static/trpl-ng.js
+++ tripel/static/trpl-ng.js
@@ -1,11 +1,12 @@
 var trplApp = angular.module('trpl', ['ngResource', 'ui.state', 'jm.i18next']);
 
-//TODO: should avoid also defining rootPath here, since it's already defined in python
-var tripelRootPath = '/tripel'
-var i18nOptions = {fallbackLng: 'en-US', resStore: window.trplLocaleMsgs, dynamicLoad: false}
-i18n.init(i18nOptions);
+i18n.init({fallbackLng: 'en-US', 
+			resStore: window.trplLocaleMsgs, 
+			dynamicLoad: false});
 
-trplApp.value('trplConstants', {rootPath: tripelRootPath});
+//TODO: should avoid also defining rootPath here, since it's already defined in python
+trplApp.value('trplConstants', {rootPath: '/tripel',
+								dateFormat: 'yyyy-MM-dd HH:mm:ss Z'});
 
 trplApp.config(
 	function($stateProvider, $routeProvider, $urlRouterProvider, $i18nextProvider) {
@@ -34,11 +35,12 @@ trplApp.config(
 				})
 			.state('appView.metaspaceCmds.nodespaceListAll.viewNodespace', {
 					url: '/:nodespaceId',
-					controller: 'NodespaceViewCtrl',
-					templateUrl: 'static/ng_partials/nodespace_graph_view.html'
+					controller: '',
+					templateUrl: 'static/ng_partials/nodespace_view.html'
 				})
 			.state('appView.metaspaceCmds.userListAll', {
 					url: '/users',
+					controller: 'UserListAllCtrl',
 					templateUrl: 'static/ng_partials/user_list_metaspace.html'
 				})
 			.state('appView.manageLoggedInUser', {
@@ -47,10 +49,12 @@ trplApp.config(
 				})
 			.state('appView.manageLoggedInUser.editUserInfo', {
 					url: '/user_info_edit',
+					controller: 'EditUserInfoCtrl',
 					templateUrl: 'static/ng_partials/user_info_edit.html'
 				})
 			.state('appView.manageLoggedInUser.changeUserPassword', {
 					url: '/user_change_password',
+					controller: 'ChangePasswordCtrl',
 					templateUrl: 'static/ng_partials/user_change_password.html'
 				})
 			.state('appView.nodespaceListAccessible', {
@@ -64,8 +68,13 @@ trplApp.config(
 				})
 			.state('appView.nodespaceListAccessible.viewNodespace', {
 					url: '/:nodespaceId',
-					controller: 'NodespaceViewCtrl',
-					templateUrl: 'static/ng_partials/nodespace_graph_view.html'
+					controller: '',
+					templateUrl: 'static/ng_partials/nodespace_view.html'
+				})
+			.state('appView.nodespaceListAccessible.viewNodespace.browseNodes', {
+					url: '/browse_nodes',
+					controller: 'NodespaceBrowseCtrl',
+					templateUrl: 'static/ng_partials/nodespace_browse_nodes.html'
 				});
 		
 		$urlRouterProvider.otherwise('/app_view/nodespaces_accessible');
@@ -75,7 +84,9 @@ trplApp.config(
 trplApp.service('trplBackendSvc',
 	function($http, trplConstants) {
 		this.isAllowedToUse = function(pageName, callbackFn) {
-			$http.get(trplConstants.rootPath+'/'+pageName, {params: {modeselektion: 'check_is_allowed_to_use'}})
+			var reqParams = {modeselektion: 'check_is_allowed_to_use'};
+			var reqUri = trplConstants.rootPath+'/'+pageName;
+			$http.get(reqUri, {params: reqParams})
 				.success(function(data, status, headers, config) {
 					var isAllowedToUse = false;
 					if(data.is_allowed_to_use !== undefined) isAllowedToUse = (data.is_allowed_to_use === true);
@@ -85,23 +96,63 @@ trplApp.service('trplBackendSvc',
 					callbackFn(false);
 				});
 		};
-
-		this.getObjList = function(callbackFn, subUri, params) {
-			reqParams = jQuery.extend({}, params, {modeselektion: 'json'});
-			reqUri = trplConstants.rootPath + subUri;
-			$http.get(reqUri, {params: reqParams})
-				.success(function(data, status, headers, config) {
-					if((data.length !== undefined)) {
-						callbackFn(data);
-					} else {
-						callbackFn([]);
-					}
-				})
-				.error(function(data, status, headers, config) {
+		
+		this.httpReq = function(reqMethod, subUrl, params, successCallbackFn, errorCallbackFn) {
+			var reqParams = jQuery.extend({}, params, {modeselektion: 'json'});
+			var reqUrl = trplConstants.rootPath + subUrl;
+			
+			var reqConfig = {method: reqMethod, url: reqUrl};
+			if(reqMethod == 'POST') {
+			//by default, angular sends data over as json, we want the more traditional form encoding.
+			//see also:  http://stackoverflow.com/questions/11442632/how-can-i-make-angular-js-post-data-as-form-data-instead-of-a-request-payload
+			//           http://victorblog.com/2012/12/20/make-angularjs-http-service-behave-like-jquery-ajax/
+				reqConfig['data'] = $.param(reqParams);
+				reqConfig['headers'] = {'Content-Type': 'application/x-www-form-urlencoded'};
+			} else {
+				reqConfig['params'] = reqParams;
+			}
+			
+			$http(reqConfig)
+				.success(successCallbackFn)
+				.error(errorCallbackFn);
+		}
+		
+		this.reqObjList = function(reqMethod, callbackFn, subUrl, params) {
+			var successCallbackFn = function(respData, status, headers, config) {
+				if((respData.length !== undefined)) {
+					callbackFn(respData);
+				} else {
 					callbackFn([]);
-				});
+				}
+			};
+			
+			var errorCallbackFn = function(respData, status, headers, config) {
+				callbackFn(null);
+			};
+			
+			this.httpReq(reqMethod, subUrl, params, successCallbackFn, errorCallbackFn);
 		};
-
+		
+		this.reqObj = function(reqMethod, callbackFn, subUrl, params) {
+			var reqCallbackFn = function(respData, status, headers, config) {
+				callbackFn(respData);
+			};
+			
+			this.httpReq(reqMethod, subUrl, params, reqCallbackFn, reqCallbackFn);
+		}
+		
+		this.getObjList = function(callbackFn, subUrl, params) {
+			this.reqObjList('GET', callbackFn, subUrl, params);
+		};
+		
+		this.getObj = function(callbackFn, subUrl, params) {
+			this.reqObj('GET', callbackFn, subUrl, params);
+		};
+		
+		this.postReq = function(callbackFn, subUrl, params) {
+			this.reqObj('POST', callbackFn, subUrl, params);
+		}
+		
 		this.getAccessibleNodespaces = function(callbackFn) {
 			return this.getObjList(callbackFn, '/nodespace_list_accessible', {});
 		};
@@ -113,11 +164,35 @@ trplApp.service('trplBackendSvc',
 		this.getGraphElements = function(callbackFn, nodespaceId) {
 			return this.getObjList(callbackFn, '/nodespace_overview', {nodespace_id: nodespaceId});
 		};
+		
+		this.getAllUsers = function(callbackFn) {
+			return this.getObjList(callbackFn, '/user_list_all', {});
+		};
+		
+		this.getNodespaceViewInfo = function(callbackFn, nodespaceId) {
+			return this.getObj(callbackFn, '/nodespace_view', {nodespace_id: nodespaceId});
+		};
+		
+		this.getAuthStatus = function(callbackFn) {
+			return this.getObj(callbackFn, '/auth_status', {});
+		};
+		
+		this.getUserViewInfo = function(callbackFn, viewedUserId) {
+			return this.getObj(callbackFn, '/user_view', {viewed_user_id: viewedUserId});
+		};
+		
+		this.updateUserInfo = function(callbackFn, userUpdData) {
+			return this.postReq(callbackFn, '/user_info_edit', userUpdData);
+		};
+		
+		this.updateUserPassword = function(callbackFn, pwUpdData) {
+			return this.postReq(callbackFn, '/user_change_pass', pwUpdData);
+		};
 	}
 );
 
 trplApp.service('paneListSvc', 
-	function(trplBackendSvc) {
+	function(trplBackendSvc, $stateParams) {
 		this.getPaneList = function(paneListType) {
 			switch(paneListType) {
 				//TODO: urlBase values should come from state info so we're not repeating the definition here
@@ -149,7 +224,24 @@ trplApp.service('paneListSvc',
 					var panes = [{title: i18n.t('user_info_edit_form_page_name'), url: urlBase+'user_info_edit', isSelected: false, isUsable: true},
 							{title: i18n.t('user_change_pass_form_page_name'), url: urlBase+'user_change_password', isSelected: false, isUsable: true}];
 					
-					return panes
+					return panes;
+				
+				case 'nodespace-view':
+					var urlBase = '#/app_view/nodespaces_accessible/' + $stateParams.nodespaceId + '/';
+					var panes = [{title: i18n.t('nodespace_browse_tab_label'), url: urlBase+'browse_nodes', isSelected: false, isUsable: true},
+								{title: i18n.t('nodespace_edit_form_page_name'), url: urlBase+'nodespace_info_edit', isSelected: false, isUsable: false},
+								{title: i18n.t('nodespace_user_list_tab_label'), url: urlBase+'user_list_nodespace', isSelected: false, isUsable: false},
+								{title: i18n.t('nodespace_invite_create_form_page_name'), url: urlBase+'nodespace_invite_create', isSelected: false, isUsable: false}];
+					
+					var callbackFn = function(nodespaceViewCmdPerms) {
+						panes[1].isUsable = nodespaceViewCmdPerms['is_allowed_to_edit_nodespace'];
+						panes[2].isUsable = nodespaceViewCmdPerms['is_allowed_to_list_nodespace_users'];
+						panes[3].isUsable = nodespaceViewCmdPerms['is_allowed_to_invite_nodespace_users'];
+					}
+					
+					trplBackendSvc.getNodespaceViewInfo(callbackFn, $stateParams.nodespaceId);
+					
+					return panes;
 					
 				default:
 					return [];
@@ -180,7 +272,7 @@ trplApp.controller('NodespaceListAllCtrl',
 		var nodespaceListData = $scope.nodespaceListData = {};
 		var callbackFn = function(newNSList) {
 			nodespaceListData.nodespaceList = newNSList;
-		}
+		};
 		trplBackendSvc.getAllNodespaces(callbackFn);
 	}
 );
@@ -192,20 +284,116 @@ trplApp.controller('NodespaceListAccessibleCtrl',
 		var nodespaceListData = $scope.nodespaceListData = {};
 		var callbackFn = function(newNSList) {
 			nodespaceListData.nodespaceList = newNSList;
-		}
+		};
 		trplBackendSvc.getAccessibleNodespaces(callbackFn);
 	}
 );
 
-trplApp.controller('NodespaceViewCtrl',
+trplApp.controller('NodespaceBrowseCtrl',
 	function($scope, $stateParams, trplBackendSvc) {
 		var nodespaceViewData = $scope.nodespaceViewData = {};
 		var callbackFn = function(newGraphElements) {
 			nodespaceViewData.graphElements = newGraphElements;
 			var nsGraph = angular.element("#ns_graph").cytoscape("get");
 			nsGraph.load(nodespaceViewData.graphElements);
-		}
+		};
 		trplBackendSvc.getGraphElements(callbackFn, $stateParams.nodespaceId);
+	}
+);
+
+trplApp.controller('UserListAllCtrl',
+	function($scope, $stateParams, trplBackendSvc, trplConstants, $filter) {
+		$scope.urlBase = '#/app_view/metaspace_cmds/users/';
+		$scope.dateFormat = trplConstants.dateFormat;
+		
+		var userListData = $scope.userListData = [];
+		var callbackFn = function(userList) {
+			userListData.userList = userList;
+			
+			userList.forEach(function(val, idx, arr) {
+								//TODO: get rid of this hack:  not sure why empty date fields come back as 'None' (the 
+								// fields in the json objects are null in the response from tripel_web).
+								if (val.creation_date === 'None') {
+									val.creation_date = null;
+								} else {
+									val.creation_date = new Date(val.creation_date.replace(" ", "T"));
+									val.creation_date = $filter('date')(val.creation_date, trplConstants.dateFormat);
+								}
+								if(val.modification_date === 'None') {
+									val.modification_date = null;
+								} else {
+									val.modification_date = new Date(val.modification_date.replace(" ", "T"));
+									val.modification_date = $filter('date')(val.modification_date, trplConstants.dateFormat);
+								}
+							});
+							
+		};
+		trplBackendSvc.getAllUsers(callbackFn);
+	}
+);
+
+trplApp.controller('EditUserInfoCtrl',
+	function($scope, trplBackendSvc) {
+		var userInfo = $scope.userInfo = {};
+		var updateStatus = $scope.updateStatus = {'encounteredUpdateError': null};
+		
+		var userInfoCallbackFn = function(userInfoResult) {
+			userInfo = $scope.userInfo = userInfoResult;
+		};
+		
+		var authStatusCallbackFn = function(authInfo) {
+			trplBackendSvc.getUserViewInfo(userInfoCallbackFn, authInfo.user_id);
+		};
+		
+		var userUpdateCallbackFn = function(updResult) {
+			if (updResult == null) {
+				updateStatus.encounteredUpdateError = true;
+			} else {			
+				updateStatus.encounteredUpdateError = Boolean(updResult.encountered_update_error);
+			}
+		};
+		
+		var submitEditForm = function() {
+			var userUpdData = {'edited_user_id': userInfo.user_id,
+								'username': userInfo.username,
+								'email_addr': userInfo.email_addr,
+								'user_statement': userInfo.user_statement};
+			trplBackendSvc.updateUserInfo(userUpdateCallbackFn, userUpdData);
+		};
+		$scope.submitEditForm = submitEditForm;
+		
+		//the fn call to get the user info will be invoke by this call to get the auth info
+		trplBackendSvc.getAuthStatus(authStatusCallbackFn);
+	}
+);
+
+trplApp.controller('ChangePasswordCtrl',
+	function($scope, trplBackendSvc) {
+		var pwInfo = $scope.pwInfo = {};
+		var updateStatus = $scope.updateStatus = {'encounteredUpdateError': null};
+		
+		var authStatusCallbackFn = function(authInfo) {
+			pwInfo.edited_user_id = authInfo.user_id;
+		};
+		trplBackendSvc.getAuthStatus(authStatusCallbackFn);
+		
+		var passwordUpdateCallbackFn = function(updResult) {
+			if (updResult == null) {
+				updateStatus.encounteredUpdateError = true;
+			} else {			
+				updateStatus.encounteredUpdateError = Boolean(updResult.encountered_update_error);
+				updateStatus.errorMsg = updResult.error_msg;
+			}
+		};
+		
+		var submitPasswordChangeForm = function() {
+			var pwUpdData = {'edited_user_id': pwInfo.edited_user_id,
+								'editing_user_cleartext_password': pwInfo.editing_user_cleartext_password,
+								'cleartext_password_1': pwInfo.cleartext_password_1,
+								'cleartext_password_2': pwInfo.cleartext_password_2};
+			trplBackendSvc.updateUserPassword(passwordUpdateCallbackFn, pwUpdData);
+		};
+		$scope.submitPasswordChangeForm = submitPasswordChangeForm;
 	}
 );
 
